5a2a53bde624f0f93881dd330b955c3f9a7c56ce
Ben Smith <bh2smith@gmail.com>
2022-11-28 12:21:07 +0100

    try localhost

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from pyhive import hive
 
 def connect():
     return hive.Connection(
-        host="jdbc:postgresql://hive-metastore/metastore",
+        host="localhost",
         port=10000,
         username="hive",
         # password="hive",

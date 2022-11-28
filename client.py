f31fe616925c680123739363685218d69e4b7aad
Ben Smith <bh2smith@gmail.com>
2022-11-28 12:33:16 +0100

    try alternative connection

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 from pyhive import hive
+import pyhs2
 
 
 def connect():
@@ -10,5 +11,32 @@ def connect():
     )
 
 
+def alt_connect():
+    with pyhs2.connect(
+        host="localhost",
+        port=10000,
+        authMechanism="PLAIN",
+        user="hive",
+        password="hive",
+        database="default",
+    ) as conn:
+        with conn.cursor() as cur:
+            # Show databases
+            print(cur.getDatabases())
+
+            # Execute query
+            cur.execute("select * from table")
+
+            # Return column info from query
+            print(cur.getSchema())
+
+            # Fetch table results
+            for i in cur.fetch():
+                print(i)
+
+
 if __name__ == "__main__":
-    connect()
+    try:
+        connect()
+    except Exception:
+        alt_connect()

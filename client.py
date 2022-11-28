dced995a022585c01c7e310ce71e997dff398a29
Ben Smith <bh2smith@gmail.com>
2022-11-28 11:09:46 +0100

    try independent images

---
+++ client.py
@@ -0,0 +1,13 @@
+from pyhive import hive
+
+def connect():
+    return hive.Connection(
+        host="jdbc:postgresql://hive-metastore-postgresql/metastore",
+        port=9083,
+        username="hive",
+        # password="hive",
+    )
+
+
+if __name__ == "__main__":
+    connect()

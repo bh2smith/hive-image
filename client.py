53183aad73bc6b83ea983d387f0523bef7403f80
Ben Smith <bh2smith@gmail.com>
2022-11-28 12:43:18 +0100

    try with all defaults

--- client.py
+++ client.py
@@ -1,42 +1,16 @@
 from pyhive import hive
-import pyhs2
 
 
 def connect():
-    return hive.Connection(
-        host="localhost",
-        port=9083,
-        username="hive",
-        # password="hive",
-    )
-
-
-def alt_connect():
-    with pyhs2.connect(
-        host="localhost",
-        port=10000,
-        authMechanism="PLAIN",
-        user="hive",
-        password="hive",
-        database="default",
-    ) as conn:
-        with conn.cursor() as cur:
-            # Show databases
-            print(cur.getDatabases())
-
-            # Execute query
-            cur.execute("select * from table")
-
-            # Return column info from query
-            print(cur.getSchema())
-
-            # Fetch table results
-            for i in cur.fetch():
-                print(i)
+    # return hive.Connection(
+    #     host="localhost",
+    #     port=9083,
+    #     username="hive",
+    #     # password="hive",
+    # )
+    return hive.connect("localhost").cursor()
 
 
 if __name__ == "__main__":
-    try:
-        connect()
-    except Exception:
-        alt_connect()
+    cur = connect()
+    print(cur)
